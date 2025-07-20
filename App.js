136ccc5a2bb5a8256a7302aa20e957e4474d0996
Letlotlo Manake <letlotlomanake31@gmail.com>
2025-07-20 20:00:17 +0200

    Create App.js
    Import and use the component in the application

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Glossary from './components/Glossary';
+
+function App() {
+  return (
+    <div className="App">
+      <Glossary />
+    </div>
+  );
+}
+
+export default App;

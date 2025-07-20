becf15ae8ce65f6db302d4587d3a9c08a626be32
Letlotlo Manake <letlotlomanake31@gmail.com>
2025-07-20 19:53:42 +0200

    Create groupedGlossary.js
    Created the GroupedGlossary CAPS-Aligned File.
    
    We can replace the [] later with terms from CAPS content

---
+++ public/src/data/groupedGlossary.js
@@ -0,0 +1,46 @@
+const groupedGlossary = [
+  {
+    subject: "Mathematics",
+    grade: "10",
+    topics: [
+      {
+        name: "Algebraic Expressions",
+        terms: [
+          {
+            term: "Coefficient",
+            definition: "A number that multiplies a variable.",
+            translations: {
+              af: "Koëffisiënt",
+              zu: "Inani elihambisana nezinguquko",
+              nso: "Motšhene",
+              ve: "Mukandandila"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    subject: "Life Sciences",
+    grade: "11",
+    topics: [
+      {
+        name: "Osmosis",
+        terms: [
+          {
+            term: "Osmosis",
+            definition: "Water movement through a semi-permeable membrane.",
+            translations: {
+              af: "Osmose",
+              zu: "Ukudlula kwamanzi",
+              nso: "Osmose",
+              ve: "Osmosisi"
+            }
+          }
+        ]
+      }
+    ]
+  }
+];
+
+export default groupedGlossary;
